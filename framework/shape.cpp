dbf7bfcc38d591916bf66a64889064221999256d
Antonia K <antonia.krumieva@gmail.com>
2020-07-19 23:53:43 +0300

    aufgabe 5.4

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -3,3 +3,11 @@
 Shape::Shape(std::string name, Color const& color) : 
     name_{name},
     color_{color} {};
+
+std::ostream& Shape::print(std:: ostream& os) const {
+    os << "Name: " << name_ << "\nColor: " << color_ << "\n";
+};
+
+std::ostream& operator<<(std::ostream& os, Shape const& s) {
+    s.print(os);
+};
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -11,9 +11,12 @@ class Shape {
     virtual float area() const = 0;
     virtual float volume() const = 0;
     Shape(std::string name, Color const& color);
+    virtual std::ostream& print(std:: ostream& os) const;
     private:
     std::string name_;
     Color color_;
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s);
 
 #endif 
